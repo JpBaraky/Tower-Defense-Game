97c8c927d6722a5e3c94a6d740da9b4aab0be38b
JpBaraky <jp.baraky@gmail.com>
2025-10-16 15:25:27 -0300

    Basic Wave Manager
    -Now there is a basic wave manager that keeps waves coming in a certain interval after all enemies are dead, and you can scale then too, with health, number of enemies and speed

---
+++ Assets/Scripts/Enemies/WaveManager/Wave.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public int enemyCount = 5;
+    public float spawnInterval = 1.5f;
+    public float enemyHealthMultiplier = 1f;
+    public float enemySpeedMultiplier = 1f;
+}
